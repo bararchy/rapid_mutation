74709f40a0b806fba89c330275b0bbd8e9e3fa04
Bar Hofesh <bar.hofesh@gmail.com>
2018-05-02 06:54:29 +0300

    Main even is initalized class, player, background and instance vars

--- src/rapid_mutation/player.cr
+++ src/rapid_mutation/player.cr
@@ -2,5 +2,37 @@ require "./basic_char/basic_char.cr"
 
 module RapidMutation
   class Player < BasicChar
+    def continous_movment
+      # Right
+      while SF::Keyboard.key_pressed?(SF::Keyboard::D)
+        player.move(:right)
+        if SF::Keyboard.key_pressed?(SF::Keyboard::S)
+          player.move(:down)
+        elsif SF::Keyboard.key_pressed?(SF::Keyboard::W)
+          player.move(:up)
+        end
+        draw(player, background)
+      end
+      # Down
+      while SF::Keyboard.key_pressed?(SF::Keyboard::S)
+        player.move(:down)
+        if SF::Keyboard.key_pressed?(SF::Keyboard::A)
+          player.move(:left)
+        elsif SF::Keyboard.key_pressed?(SF::Keyboard::D)
+          player.move(:right)
+        end
+        draw(player, background)
+      end
+      # Right
+      while SF::Keyboard.key_pressed?(SF::Keyboard::A)
+        player.move(:left)
+        if SF::Keyboard.key_pressed?(SF::Keyboard::S)
+          player.move(:down)
+        elsif SF::Keyboard.key_pressed?(SF::Keyboard::W)
+          player.move(:up)
+        end
+        draw(player, background)
+      end
+    end
   end
 end
